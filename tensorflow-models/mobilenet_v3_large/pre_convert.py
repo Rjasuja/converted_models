e8b4b172871f8aa5ebf1bec81593a6d6a8b8c9ca
Ritul Jasuja <ritul.jasuja@intel.com>
2022-03-14 16:59:52 +0530

    first commit

---
+++ tensorflow-models/mobilenet_v3_large/pre_convert.py
@@ -0,0 +1,22 @@
+import argparse
+import tensorflow as tf
+
+from pathlib import Path
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_dir', type=Path)
+    parser.add_argument('output_dir', type=Path)
+    args = parser.parse_args()
+
+    tf.keras.backend.set_image_data_format('channels_last')
+
+    model = tf.keras.applications.MobileNetV3Large(
+        weights='./weights_mobilenet_v3_large_224_1.0_float.h5'
+#        weights=str(args.input_dir/'weights_mobilenet_v3_large_224_1.0_float.h5')
+    )
+    model.save(filepath='./mobilenet_v3_large_224_1.0_float.savedmodel')
+
+
+if __name__ == '__main__':
+    main()
